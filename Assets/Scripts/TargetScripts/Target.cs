2804bb6dbb67e97ac7a15dfbfd9d6b5e815e9235
bilal asghar <asgharbilal8@gmail.com>
2021-12-25 00:42:29 +0000

    started simple target spawner with algorithm to increase number of targets as rounds progress

---
+++ Assets/Scripts/TargetScripts/Target.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public NavMeshAgent TargetAgent;
+    public Transform Player;
+
+    public LayerMask IsPlayer;
+    public LayerMask IsGround;
+    public float TargetHealth = 1000f;
+    public float AttackRange;
+    public float TimeBetweenAttacks;
+    public float TargetDamage;
+    public bool PlayerInAttackRange;
+    public bool AlreadyAttacked;
+
+
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        Player = FindObjectOfType<PlayerController>().transform;
+        _playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Start()
+    {
+        TargetHealth = 1000f;
+        TargetDamage = 100f;
+    }
+
+    public void TargetTakeDamage(float damage)
+    {
+        TargetHealth -= damage;
+
+        bool targetHasNoHealth = TargetHealth <= 0;
+        if (targetHasNoHealth)
+        {
+            TargetDie();
+        }
+    }
+
+    private void TargetDie()
+    {
+        Debug.Log("Killed Object");
+        Destroy(gameObject);
+    }
+
+    private void Movement()
+    {
+        TargetAgent.SetDestination(Player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Stop moving
+        TargetAgent.SetDestination(transform.position);
+
+        transform.LookAt(Player);
+
+        //if hasn't attacked then attack
+        if (!AlreadyAttacked)
+        {
+            AlreadyAttacked = true;
+            _playerController.PlayerTakeDamage(damage: TargetDamage);
+            Invoke(nameof(ResetAttack), TimeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        AlreadyAttacked = false;
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerInAttackRange = Physics.CheckSphere(transform.position, AttackRange, IsPlayer);
+
+        if (PlayerInAttackRange)
+        {
+            AttackPlayer();
+        }
+        Movement();
+    }
+}
+
+
+
+
+/*
+- start of with 100 health (base health)
+- increase by 100 every round up till round 10
+- once round 10 starts to increase by 10 percent
+ */
---
+++ Assets/Scripts/TargetScripts/TargetSpawner.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    public int RoundNumber = 1;
+    public bool ShouldSpawn = true;
+    public GameObject TargetObject;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (ShouldSpawn)
+        {
+            while (transform.childCount < NumberOfTargets())
+            {
+                Instantiate(TargetObject, transform);
+                ShouldSpawn = false;
+            }
+        }
+
+        Debug.Log(transform.childCount);
+    }
+
+    private double NumberOfTargets()
+    {
+        double preciseNumberOfTargets = 0.000058 * Mathf.Pow(RoundNumber, 3) + 0.074032 * Mathf.Pow(RoundNumber, 2) + 0.718119 * RoundNumber + 14.738699;
+        double numberOfTargetsRounded = Mathf.Round((float)preciseNumberOfTargets);
+        Debug.Log(preciseNumberOfTargets);
+        return numberOfTargetsRounded;
+    }
+}
+
+
+/*
+ 
+--R = current round number
+
+1 player: 0,000058 * R^3 + 0,074032 * R^2 + 0,718119 *R + 14,738699
+
+2 players: 0,000054 * R^3 + 0,169717 * R^2 + 0,541627 *R + 15,917041
+
+3 players: 0,000169 * R^3 + 0,238079 * R^2 + 1,307276 *R + 21,291046
+
+4 players: 0,000225 * R^3 + 0,314314 * R^2 + 1,835712 *R + 27,596132
+ 
+ */
